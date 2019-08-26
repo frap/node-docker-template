bce3e40f1471bd04b4a4372d4b090e95d6163ca6
Andrés Gasson <agasson@ateasystems.com>
2019-08-26 14:22:42 +1200

    :art: Read as clone project stage 1
    added ramda as dep with a test

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -1,17 +1,16 @@
 const sum = require('../src/sum');
 const R = require('ramda');
 
-
-let nums = [33 ,11, 5, 8, 2,15 ];
+let nums = [33, 11, 5, 8, 2, 15];
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
-describe("Ramda", () => {
-    it("add via sum is equal to R.add(1,2)", () => {
-        expect(sum(1, 2)).toBe(R.add(1,2));
-    });
-    test('with Math.min works on array', () => {
+describe('Ramda', () => {
+  it('add via sum is equal to R.add(1,2)', () => {
+    expect(sum(1, 2)).toBe(R.add(1, 2));
+  });
+  test('with Math.min works on array', () => {
     expect(R.apply(Math.min, nums)).toBe(2);
-    });
+  });
 });
